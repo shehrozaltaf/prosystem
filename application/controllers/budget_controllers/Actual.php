9255f0dceacf5b352e40ed67b8622593d42d0f50
shahroz.khan <shehroz.altaf92@gmail.com>
2020-10-13 14:33:05 +0500

    budget system

--- application/controllers/budget_controllers/Actual.php
+++ application/controllers/budget_controllers/Actual.php
@@ -36,6 +36,28 @@ class Actual extends CI_controller
         $this->load->view('include/footer');
     }
 
+    function addActual_view(){
+        $data = array();
+        /*==========Log=============*/
+        $Custom = new Custom();
+        $trackarray = array("action" => "View Project",
+            "result" => "View Project page. Fucntion: Project/index()");
+//        $Custom->trackLogs($trackarray, "user_logs");
+        /*==========Log=============*/
+        $MSettings = new MSettings();
+        $data['permission'] = $MSettings->getUserRights($_SESSION['login']['idGroup'], '',  uri_string());
+
+        $Mproject = new Mproject();
+        $data['data'] = $Mproject->getAll();
+
+        $this->load->view('include/header');
+        $this->load->view('include/top_header');
+        $this->load->view('include/sidebar');
+        $this->load->view('budget_views/actual_add', $data);
+        $this->load->view('include/customizer');
+        $this->load->view('include/footer');
+    }
+
 }
 
 ?>
--- application/controllers/budget_controllers/Projected.php
+++ application/controllers/budget_controllers/Projected.php
@@ -34,6 +34,28 @@ class Projected extends CI_controller
         $this->load->view('include/footer');
     }
 
+    function addProjected_view(){
+        $data = array();
+        /*==========Log=============*/
+        $Custom = new Custom();
+        $trackarray = array("action" => "View Project",
+            "result" => "View Project page. Fucntion: Project/index()");
+//        $Custom->trackLogs($trackarray, "user_logs");
+        /*==========Log=============*/
+        $MSettings = new MSettings();
+        $data['permission'] = $MSettings->getUserRights($_SESSION['login']['idGroup'], '',  uri_string());
+
+        $Mproject = new Mproject();
+        $data['data'] = $Mproject->getAll();
+
+        $this->load->view('include/header');
+        $this->load->view('include/top_header');
+        $this->load->view('include/sidebar');
+        $this->load->view('budget_views/projected_add', $data);
+        $this->load->view('include/customizer');
+        $this->load->view('include/footer');
+    }
+
 }
 
 ?>
