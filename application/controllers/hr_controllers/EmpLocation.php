b9333f7dbeb7526b42f843831e2fcbeaac214538
muhammad.fazal <fazal.abbas4@gmail.com>
2020-10-31 11:14:09 +0500

    Add Location View.

--- application/controllers/hr_controllers/EmpLocation.php
+++ application/controllers/hr_controllers/EmpLocation.php
@@ -17,11 +17,11 @@ class EmpLocation extends CI_Controller
         parent::__construct();
         $this->load->model('custom');
         $this->load->model('msettings');
+        $this->load->model('hr_model/mhr_settings');
         if (!isset($_SESSION['login']['idUser'])) {
             redirect(base_url());
         }
     }
-
 
     function index()
     {
@@ -35,6 +35,8 @@ class EmpLocation extends CI_Controller
             "result" => "View Dashboard Users page. Fucntion: index()");
         $Custom->trackLogs($trackarray, "user_logs");
 
+        $MHr_settings= new Mhr_settings();
+        $data['data'] = $MHr_settings->getAllLocation();
 
         $this->load->view('include/header');
         $this->load->view('include/top_header');
@@ -43,4 +45,30 @@ class EmpLocation extends CI_Controller
         $this->load->view('include/customizer');
         $this->load->view('include/footer');
     }
+
+
+
+//     function addLocation_view(){
+//         $data = array();
+//         /*==========Log=============*/
+//         $Custom = new Custom();
+//         $trackarray = array("action" => "View Project",
+//             "result" => "View Project page. Fucntion: EmpLocation/index()");
+// //        $Custom->trackLogs($trackarray, "user_logs");
+//         /*==========Log=============*/
+//         $MSettings = new MSettings();
+//         $data['permission'] = $MSettings->getUserRights($_SESSION['login']['idGroup'], '',  uri_string());
+
+//         $MHr_settings= new Mhr_settings();
+//         $data['data'] = $MHr_settings->getAllLocation();
+
+//         $this->load->view('include/header');
+//         $this->load->view('include/top_header');
+//         $this->load->view('include/sidebar');
+//         $this->load->view('hr_views/hr_setttings/emp_location', $data);
+//         $this->load->view('include/customizer');
+//         $this->load->view('include/footer');
+//     }
+
+
 }
---
+++ application/views/hr_views/hr_settings/emp_location.php
@@ -0,0 +1,198 @@
+<link rel="stylesheet" type="text/css"
+      href="<?php echo base_url() ?>assets/vendors/css/tables/datatable/datatables.min.css">
+
+<!-- BEGIN: Content-->
+<div class="app-content content">
+    <div class="content-overlay"></div>
+    <div class="header-navbar-shadow"></div>
+    <div class="content-wrapper">
+        <div class="content-header row">
+            <div class="content-header-left col-md-9 col-12 mb-2">
+                <div class="row breadcrumbs-top">
+                    <div class="col-12">
+                        <h2 class="content-header-title float-left mb-0">Location</h2>
+                        <div class="breadcrumb-wrapper col-12">
+                            <ol class="breadcrumb">
+                                <li class="breadcrumb-item">
+                                    <a href="<?php base_url() ?>">Home</a>
+                                </li>
+                                <li class="breadcrumb-item active">Location</li>
+                            </ol>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="content-body">
+            <section id="column-selectors">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="card">
+                            <div class="card-header">
+                                <h4 class="card-title">Location</h4>
+                            </div>
+                            <div class="card-content">
+                                <div class="card-body card-dashboard">
+                                    <div class="table-responsive">
+                                        <table class="table table-striped dataex-html5-selectors">
+                                            <thead>
+                                            <tr>
+                                                <th>SNo</th>
+                                                <th>Location</th>
+                                                
+                                            </tr>
+                                            </thead>
+                                            <tbody>
+                                            <?php
+                                            
+                                            $SNo = 0;
+                                            if (isset($data) && $data != '') {
+                                                foreach ($data as $v) {
+                                                    $SNo++; ?>
+                                                    <tr>
+                                                        <td><?php echo $SNo ?></td>
+                                                        <td><?php echo $v->location ?></td>
+                                                        
+                                                      
+                                                    </tr>
+                                                <?php }
+                                            } ?>
+                                            </tbody>
+                                            <tfoot>
+                                            <tr>
+                                                <th>SNo</th>
+                                                <th>Location</th>
+                                              
+                                            </tr>
+                                            </tfoot>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="md-fab-wrapper">
+                            <a class="md-fab md-fab-accent md-fab-wave-light waves-effect waves-button waves-light"
+                               href="<?php echo  base_url()?>index.php/hr_controllers/EmpLocation/addLocation_view">
+                                <i class="feather icon-plus"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    </div>
+</div>
+<!-- END: Content-->
+
+<div class="modal fade text-left" id="deleteModal" tabindex="-1" role="dialog"
+         aria-labelledby="myModalLabel_delete"
+         aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header bg-primary white">
+                    <h4 class="modal-title white" id="myModalLabel_delete">Delete Actual</h4>
+                    <input type="hidden" id="delete_idActual" name="delete_idActual">
+                </div>
+                <div class="modal-body">
+                    <p>Are you sure, you want to delete this?</p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn grey btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary" onclick="deleteData()">Delete
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+<!-- BEGIN: Page Vendor JS-->
+<script src="<?php echo base_url() ?>assets/vendors/js/tables/datatable/pdfmake.min.js"></script>
+<script src="<?php echo base_url() ?>assets/vendors/js/tables/datatable/vfs_fonts.js"></script>
+<script src="<?php echo base_url() ?>assets/vendors/js/tables/datatable/datatables.min.js"></script>
+<script src="<?php echo base_url() ?>assets/vendors/js/tables/datatable/datatables.buttons.min.js"></script>
+<script src="<?php echo base_url() ?>assets/vendors/js/tables/datatable/buttons.html5.min.js"></script>
+<script src="<?php echo base_url() ?>assets/vendors/js/tables/datatable/buttons.print.min.js"></script>
+<script src="<?php echo base_url() ?>assets/vendors/js/tables/datatable/buttons.bootstrap.min.js"></script>
+<script src="<?php echo base_url() ?>assets/vendors/js/tables/datatable/datatables.bootstrap4.min.js"></script>
+<!-- END: Page Vendor JS-->
+<!-- BEGIN: Page JS-->
+<!--<script src="--><?php //echo base_url() ?><!--assets/js/scripts/datatables/datatable.js"></script>-->
+<!-- END: Page JS-->
+
+<script>
+    $(document).ready(function () {
+        $('.dataex-html5-selectors').DataTable({
+            dom: 'Bfrtip',
+            "displayLength": 50,
+            buttons: [
+                {
+                    extend: 'copyHtml5',
+                    exportOptions: {
+                        columns: [0, ':visible']
+                    }
+                },
+                {
+                    extend: 'pdfHtml5',
+                    exportOptions: {
+                        columns: ':visible'
+                    }
+                }, {
+                    extend: 'csv',
+                    exportOptions: {
+                        columns: ':visible'
+                    }
+                },
+                {
+                    text: 'JSON',
+                    action: function (e, dt, button, config) {
+                        var data = dt.buttons.exportData();
+
+                        $.fn.dataTable.fileSave(
+                            new Blob([JSON.stringify(data)]),
+                            'Export.json'
+                        );
+                    }
+                },
+                {
+                    extend: 'print',
+                    exportOptions: {
+                        columns: ':visible'
+                    }
+                }
+            ]
+        });
+    });
+
+
+
+    function getDelete(obj) {
+        var id = $(obj).parent('td').attr('data-id');
+        $('#delete_idActual').val(id);
+        $('#deleteModal').modal('show');
+    }
+
+    function deleteData() {
+        var data = {};
+        data['idActual'] = $('#delete_idActual').val();
+        if (data['idActual'] == '' || data['idActual'] == undefined || data['idActual'] == 0) {
+            toastMsg('Group', 'Something went wrong', 'error');
+            return false;
+        } else {
+            CallAjax('<?php echo base_url('index.php/budget_controllers/Actual/deleteData')?>', data, 'POST', function (res) {
+                if (res == 1) {
+                    $('#deleteModal').modal('hide');
+                    toastMsg('Group', 'Successfully Deleted', 'success');
+                    setTimeout(function () {
+                        window.location.reload();
+                    }, 500);
+                } else if (res == 2) {
+                    toastMsg('Group', 'Something went wrong', 'error');
+                } else if (res == 3) {
+                    toastMsg('Group', 'Invalid Group', 'error');
+                }
+
+            });
+        }
+    }
+</script>
