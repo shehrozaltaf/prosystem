b5ba50b0cce0636875811d20151837e02a63d24e
javed.khan <javedak09@gmail.com>
2020-11-04 14:58:27 +0500

    HR System EmpEntry

--- application/controllers/hr_controllers/EmpPEME.php
+++ application/controllers/hr_controllers/EmpPEME.php
@@ -37,7 +37,6 @@ class EmpPEME extends CI_Controller
         $Custom->trackLogs($trackarray, "user_logs");
         
         
-
         $this->load->view('include/header');
         $this->load->view('include/top_header');
         $this->load->view('include/sidebar');
