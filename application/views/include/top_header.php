1cfeb735d1558ad32aa63d10c2fedbbcca8dc96b
muhammad.fazal <fazal.abbas4@gmail.com>
2020-11-16 15:07:00 +0500

    Add UI modal for Change Password

--- application/views/include/top_header.php
+++ application/views/include/top_header.php
@@ -42,6 +42,11 @@
                                 <i class="feather icon-user"></i> Edit Profile
                             </a>
                             <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="javascript:void(0)" onclick="changePassword()">
+                                <i class="feather icon-lock"></i>
+                                Change Password
+                            </a>
+                            <div class="dropdown-divider"></div>
                             <a class="dropdown-item" href="javascript:void(0)" onclick="logout()">
                                 <i class="feather icon-power"></i>
                                 Logout
@@ -54,3 +59,59 @@
     </div>
 </nav>
 <!-- END: Header-->
+
+<div class="modal fade text-left" id="paswordModal" tabindex="-1" role="dialog"
+         aria-labelledby=""
+         aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header bg-primary white">
+                    <h4 class="modal-title white" id="">Change Password</h4>
+                    <input type="hidden" id="delete_idActual" name="delete_idActual">
+                </div>
+                <div class="modal-body">
+                    <p>Are you sure, change password?</p>
+                </div>
+                <div class="col-sm-6 col-6">
+                     <div class="form-group">
+                           <label for="current_password" class="label-control">Currnet Password</label>
+                            <input type="text" class="form-control" id="current_password" name="current_password"
+                                 autocomplete="current_password" required>
+                        </div>
+                    </div>
+                    <div>
+                </div>
+                <div class="col-sm-6 col-6">
+                     <div class="form-group">
+                           <label for="new_password" class="label-control">New Password</label>
+                            <input type="text" class="form-control" id="new_password" name="new_password"
+                                 autocomplete="new_password" required>
+                        </div>
+                    </div>
+                    <div>
+                </div>
+                <div class="col-sm-6 col-6">
+                     <div class="form-group">
+                           <label for="confirm_password" class="label-control">Confirm Password</label>
+                            <input type="text" class="form-control" id="confirm_password" name="confirm_password"
+                                 autocomplete="confirm_password" required>
+                        </div>
+                    </div>
+                    <div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn grey btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary" onclick="">Change Password
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<script>
+function changePassword(obj) {
+        var id = $(obj).parent('td').attr('data-id');
+        $('#delete_idActual').val(id);
+        $('#paswordModal').modal('show');
+    }
+    </script>
